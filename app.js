c619f67cc3608681d1befc859d28d96bcc6900fa
Ikhsan Putra <ikhsan.putra@clem-e.com>
2020-03-23 20:45:36 +0100

    feat: use mongoose

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts'); 
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 const routeUser = require('./routes/user');
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+// mongoose
+mongoose.connect('mongodb://localhost:27017/test', 
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Mongoose connected...'))
+    .catch(err => console.error(err))
 
 // ejs
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false })); 
 
 // Routes
 app.use('/',routes);
