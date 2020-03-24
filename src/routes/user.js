01ae259f35e6fb0c06a9984c8da6b75f81694cc2
Ikhsan Putra <ikhsan.putra@clem-e.com>
2020-03-24 16:21:17 +0100

    feat: handle login, protect resource

--- src/routes/user.js
+++ src/routes/user.js
@@ -86,4 +86,11 @@ router.post('/login', (req, res, next) => {
     })(req, res, next);
 });
 
+// Logout handle
+router.get('/logout', (req, res,next) => {
+    req.logout();
+    req.flash('success_msg', "You are logged out");
+    res.redirect('/users/login');
+})
+
 module.exports = router;
