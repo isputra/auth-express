a84cefed5eba56474adc01685562d48ed2990005
Ikhsan Putra <ikhsan.putra@clem-e.com>
2020-03-24 15:02:23 +0100

    refactor: separate express into a config file

--- app.js
+++ app.js
@@ -1,49 +1,12 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const flash = require('connect-flash');
-const session = require('express-session');
-
+const app = require('./config/express');
 const mongoose = require('./config/mongoose');
-
-const routes = require('./routes');
-const routeUser = require('./routes/user');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
+const { port, env } = require('./config/vars');
 
 // mongoose
 mongoose.connect()
     .then(() => console.log('Mongoose connected...'))
     .catch(err => console.error(err))
 
-// ejs
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
-
-// for parsing application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: false })); 
-
-// express session
-app.use(session({
-    secret: 'secret-session',
-    resave: true,
-    saveUninitialized: true
-}))
-
-// flash
-app.use(flash());
-
-// Global variables
-app.use((req, res, next) => {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    next();
-});
-  
-// Routes
-app.use('/',routes);
-app.use('/users',routeUser);
-
-app.listen(PORT, () => {
-    console.log(`Listening to port ${PORT}...`)
+app.listen(port, () => {
+    console.log(`Listening to port ${port} ${env}...`)
 })
---
+++ config/express.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const routes = require('../routes');
+const routeUser = require('../routes/user');
+
+const app = express();
+
+// ejs
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false })); 
+
+// Express session
+app.use(session({
+    secret: 'secret-session',
+    resave: true,
+    saveUninitialized: true
+}))
+
+// Flash
+app.use(flash());
+
+// Global variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
+  
+// Routes
+app.use('/',routes);
+app.use('/users',routeUser);
+
+module.exports = app;
