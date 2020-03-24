2030fa469e696276111f10308d815ca12df51f9d
Ikhsan Putra <ikhsan.putra@clem-e.com>
2020-03-24 15:13:13 +0100

    refactor: move to src directory

--- src/config/vars.js
+++ src/config/vars.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 // import .env variables
 require('dotenv-safe').config({
-  path: path.join(__dirname, '../.env'),
-  sample: path.join(__dirname, '../.env.example'),
+  path: path.join(__dirname, '../../.env'),
+  sample: path.join(__dirname, '../../.env.example'),
 });
 
 module.exports = {
